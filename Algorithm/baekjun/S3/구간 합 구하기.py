0512c47f6233fd9939f0233a518c69cc9cccee92
mac <mac@macui-MacBookAir.local>
2023-07-18 01:12:18 +0900

    1d1a

---
+++ Algorithm/baekjun/S3/구간 합 구하기.py
@@ -0,0 +1,38 @@
+# n,m = map(int, input().split())
+# li = list(map(int, input().split()))
+
+
+
+# for k in range(m):
+#     inli = []
+#     i,j= map(int, input().split())
+#     answer = 0
+#     for l in range(j-i+1):
+#         answer +=  (li[i-1+l])
+#     print(answer)
+# 시간 초과됨
+
+# n,m = map(int, input().split())
+# li = list(map(int, input().split()))
+
+# for k in range(m):
+#     i, j = map(int, input().split())
+#     answer = 0
+#     h = i
+#     while h <= j:
+#         answer += li[h-1]
+#         h = h + 1
+#     print(answer)
+
+n,m = map(int, input().split())
+li = list(map(int, input().split()))
+
+for k in range(1,n):
+        li[k] += li[k-1]
+li = [0] + li
+
+for u in range(m):
+    i, j = map(int, input().split())
+    print(li[j]-li[i-1])
+
+
